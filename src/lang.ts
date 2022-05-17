2e07c7d11e340df0a06cb8a677644e189d640bd9
Christian L <cleistner@scale-it.org>
2022-05-17 14:51:30 +0200

    typescript lang

--- src/lang.ts
+++ src/lang.ts
@@ -1,6 +1,8 @@
+import { EventEmitter } from 'events';
+
 namespace lang{
     export function Main(){
-        dateSerialization();
+        useExample();
     }
 
     function urlEncoding(){
@@ -26,6 +28,56 @@ namespace lang{
             console.log("\t", d.toLocaleTimeString());
             console.log("\t", d.toUTCString());
         }
+    }
+
+    class Example extends EventEmitter{
+        private secret = 1;
+
+        example(){
+            console.log(this, this.secret);
+        }
+
+        call1(){
+            this.example();
+        }
+
+        call2(){
+            function myfunc(this: Example){
+                console.log("call2 myfunc:", this);
+                try{
+                    this.example();
+                }catch(e){
+                    console.error(e);
+                }
+            }
+            myfunc.bind(this)();
+        }
+
+        call3(){
+            const myfunc = () => {
+                this.example();
+            };
+            myfunc();
+        }
+
+        sendEvent(){
+            this.emit("data", this.secret);
+        }
+
+        on(eventName: "data", listener: (...args: any[]) => void): this {
+            return super.on(eventName, listener);
+        }
+    }
+
+    function useExample(){
+        const ex = new Example();
+        ex.call1();
+        ex.call2();
+        ex.call3();
+        ex.on("data", value => {
+            console.log("emitted:", value);
+        });
+        ex.sendEvent();
     }
 }
 
